09eab5f22146277f7c5c9f09e089700d5442af0b
Bill B <bill.blum@gmail.com>
2025-12-07 13:35:12 -0500

    Day 7 Part 2.

--- day7/day7.py
+++ day7/day7.py
@@ -24,11 +24,11 @@ def pt1_count_splits(rows):
     beams_visualized.append("".join(entry_row))
 
     # Process remaining rows
-    for row_id in tqdm(range(2, len(rows))):
+    for row_id in range(2, len(rows)):
         # Find beams from previous 
         prev_row = beams_visualized[row_id - 1]
         incoming_beams = [idx for idx, char in enumerate(prev_row) if char == beam]
-        # Find splitters in current row
+        # Find splitters in current row, and let the beams 'fall thru'
         new_row = list(rows[row_id])
         for beam_idx in incoming_beams:
             if new_row[beam_idx] == splitter:
@@ -41,11 +41,27 @@ def pt1_count_splits(rows):
         beams_visualized.append(new_row)
 
     return split_count, beams_visualized
-        
+
+def pt2_count_distinct_paths(rows):
+    splitter = '^'
+    startpos = rows[0].index('S')
+    intermediates = [0] * len(rows[0])
+    intermediates[startpos] = 1
+    
+    for row in range(1,len(rows)):
+        curr_row = rows[row]
+        splitters = [idx for idx, char in enumerate(curr_row) if char == splitter]
+        for split in splitters:
+            tmp = intermediates[split]
+            intermediates[split] = 0
+            intermediates[split+1] += tmp
+            intermediates[split-1] += tmp
+    
+    return sum(intermediates)
 
 
 if __name__ == '__main__':
-    filename = "test_input.txt"
+    # filename = "test_input.txt"
     filename = "input.txt"
 
     raw_data = read_input(filename)
@@ -53,5 +69,6 @@ if __name__ == '__main__':
     split_count, beams_in_tree = pt1_count_splits(raw_data)
 
     print(f'Pt 1: Split count = {split_count}')
+    print(f'Pt 2: Quantum Tachyons = {pt2_count_distinct_paths(raw_data)}')
 
     # print_tree(beams_in_tree)
