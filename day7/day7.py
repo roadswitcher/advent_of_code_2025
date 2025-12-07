82424f4445654b105460b60fc8b45a4a83dca7a8
Bill B <bill.blum@gmail.com>
2025-12-07 10:54:06 -0500

    Day 7 part 1, adding tqdm because Part 2 looks FUN

--- day7/day7.py
+++ day7/day7.py
@@ -1,4 +1,5 @@
 """Day 7, AOC 2025"""
+from tqdm import tqdm
 
 def read_input(filename: str) -> list:
     """Read input from file"""
@@ -11,15 +12,46 @@ def print_tree(rows):
         print(row)
 
 def pt1_count_splits(rows):
-    count = 0
+    beam = '|'
+    splitter = '^'
     beams_visualized = []
+    split_count = 0
     # find where it starts, first row
     startpos = rows[0].index('S')
     beams_visualized.append(rows[0])
-    for
+    entry_row = list(rows[1])
+    entry_row[startpos]=beam
+    beams_visualized.append("".join(entry_row))
+
+    # Process remaining rows
+    for row_id in tqdm(range(2, len(rows))):
+        # Find beams from previous 
+        prev_row = beams_visualized[row_id - 1]
+        incoming_beams = [idx for idx, char in enumerate(prev_row) if char == beam]
+        # Find splitters in current row
+        new_row = list(rows[row_id])
+        for beam_idx in incoming_beams:
+            if new_row[beam_idx] == splitter:
+                new_row[beam_idx-1] = beam
+                new_row[beam_idx+1] = beam
+                split_count += 1
+            else:
+                new_row[beam_idx] = beam
+        new_row = "".join(new_row)
+        beams_visualized.append(new_row)
+
+    return split_count, beams_visualized
+        
+
+
 if __name__ == '__main__':
     filename = "test_input.txt"
+    filename = "input.txt"
 
     raw_data = read_input(filename)
 
-    print_tree(raw_data)
+    split_count, beams_in_tree = pt1_count_splits(raw_data)
+
+    print(f'Pt 1: Split count = {split_count}')
+
+    # print_tree(beams_in_tree)
